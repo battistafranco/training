01243ece1f7b78678d554cc243a5d53d941c2b2b
Guillermo Albarello <galbarello@gmail.com>
2016-05-24 22:57:11 -0300

    serialization

---
+++ Messages/BinarySerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Messages
+{
+	public class BinarySerializer:ISerializer
+	{
+		readonly BinaryFormatter _formatter= new BinaryFormatter();
+
+		public void WriteMessage (object message, Type type, Stream stream)
+		{
+			_formatter.Serialize (stream,message);
+		}
+
+		public object ReadMessage (Stream stream)
+		{
+			return _formatter.Deserialize (stream);
+		}
+	}
+}
+
---
+++ Messages/ISerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace Messages
+{
+	public interface ISerializer
+	{
+		void WriteMessage(object message, Type type, Stream stream);
+		object ReadMessage (Stream stream);
+	}
+}
+
