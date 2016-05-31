03666b8fc1d04627d41fc2e39fb56f465d3e1e71
Guillermo Albarello <guillermo@moravia.com>
2016-05-31 15:47:45 -0300

    in-memory queue

---
+++ Messages/Queuing/Infraestructure.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Queuing
+{
+  public static class Infraestructure
+  {
+    public static void Print(string message)
+    {
+      // just printing messages nicely
+      // and without spaces in the beginning
+      var oldColor = Console.ForegroundColor;
+      foreach (var line in message.Split(new[] { Environment.NewLine }, StringSplitOptions.None))
+      {
+        var trimmed = line.TrimStart();
+
+        if (trimmed.StartsWith("#"))
+        {
+          Console.ForegroundColor = ConsoleColor.DarkRed;
+        }
+        else if (trimmed.StartsWith("*") | trimmed.StartsWith("- "))
+        {
+          Console.ForegroundColor = ConsoleColor.DarkBlue;
+        }
+        else
+        {
+          Console.ForegroundColor = ConsoleColor.White;
+        }
+        
+        Console.WriteLine(trimmed);
+      }
+      Console.ForegroundColor = oldColor;
+    }  
+  }
+}
---
+++ Messages/Queuing/RemoveProductFromBasketMessage.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Queuing
+{
+  public class RemoveProductFromBasketMessage
+  {
+    public readonly string Name;
+    public readonly double Quantity;
+
+    public RemoveProductFromBasketMessage(string name, double quantity)
+    {
+      Name= name;
+      Quantity = quantity;
+    }
+  }
+}
